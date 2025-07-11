be06a032e240733b3c5912a9d3abe035540e4a61
ramkumar0096 <rkum0046@gmail.com>
2025-07-11 22:38:07 +0530

    first commit

---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import VerticalCard from '../component/VerticalCard'
+
+const Product = () => {
+  return (
+    <div className='flex gap-2'>
+        <VerticalCard/>
+    </div>
+  )
+}
+
+export default Product
