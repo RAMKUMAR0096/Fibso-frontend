ae07260493350abc6f76cfc4a3d5025ad3f2af7a
ramkumar0096 <rkum0046@gmail.com>
2025-07-12 16:17:08 +0530

    filter and contact page integration

--- frontend/src/route/index.jsx
+++ frontend/src/route/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
 import Product from "../pages/Product.jsx";
+import Contact from "../pages/Contact.jsx";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path : "/product",
                 element : <Product/>
+            },
+            {
+                path : "/contact",
+                element : <Contact />
             }
         ]
     }
